db37e34197519b50f319d9feb6f155dff6bc944f
Victor Lellis <vmlellis@gmail.com>
2016-09-20 21:55:34 -0300

    Read from YAML

--- read-yaml/read-yaml.go
+++ read-yaml/read-yaml.go
@@ -5,13 +5,27 @@ import (
   "os"
   "bytes"
   "io/ioutil"
-  //"gopkg.in/yaml.v2"
+  "gopkg.in/yaml.v2"
+  "github.com/davecgh/go-spew/spew"
 )
 
+type Host struct {
+  Host string     `yaml:"host"`
+  Port int        `yaml:"port"`
+  User string     `yaml:"user"`
+  Password string `yaml:"password"`
+  scheme string   `yaml:"scheme"`
+}
+
+type Config struct {
+    Hosts []Host `yaml:"hosts"`
+}
+
+
 func check(e error) {
-    if e != nil {
-        panic(e)
-    }
+  if e != nil {
+    panic(e)
+  }
 }
 
 func readConfig() string {
@@ -25,6 +39,26 @@ func readConfig() string {
 }
 
 func main() {
-  config := readConfig()
+  configText := readConfig()
+  fmt.Println(configText)
+  config := Config{}
+  err := yaml.Unmarshal([]byte(configText), &config)
+  check(err)
+  spew.Dump(config)
   fmt.Println(config)
+  fmt.Println(config.Hosts)
+  fmt.Println(config.Hosts[0].Host)
+  fmt.Println(config.Hosts[1].Host)
+
+  m := make(map[string][]map[string]interface{})
+  err = yaml.Unmarshal([]byte(configText), &m)
+  check(err)
+  spew.Dump(m)
+  hosts := m["hosts"]
+
+  for _, host := range hosts {
+    for k, v := range host {
+      fmt.Printf("%s -> %s\n", k, v)
+    }
+  }
 }
